eaf312899d376ff35b626ec573d7aae816036037
SohamSinghal <sohamsinghal32@gmail.com>
2021-06-24 15:27:14 +0530

    Captions Added

--- YouTube_Download.py
+++ YouTube_Download.py
@@ -1,14 +1,31 @@
 from pytube import YouTube
-SavePath = "~/Videos"
+SavePath = "/home/soham/YouTube Downloads"
 print("Add links of video to the \'links.txt\' file")
 file = open('links.txt','r')
 links = file.read().split(',')
 for i in links:
     #Check whether link valid or not
     try:
-        yt = YouTube(i)
+        yt = YouTube(i) #yt is an object
     except:
         print("Invalid Link")
         continue
 #Print Tittle of the tittle
-    print(yt.title)
+    title = yt.title
+    print(title)
+    stream = yt.streams.get_by_resolution("480p")
+    if stream == None:
+        stream = yt.streams.get_highest_resolution()
+    stream.download(output_path=SavePath,max_retries=1)
+    caption = yt.captions.get_by_language_code('en')
+    if caption == None:
+        print("No Caption")
+    else:
+        srt_captions = caption.generate_srt_captions()
+        subtitles = open(SavePath+"/"+title+".txt","w")
+        subtitles.write(srt_captions)
+        subtitles.close()
+        print("English Caption There")
+        print()
+print("Done")
+
